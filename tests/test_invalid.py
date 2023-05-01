d664d7c408ab267995cb715fe9c8ee88eba70e71
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2023-05-01 10:08:18 +0100

    Switched Invalid data tests from pytest to unittest

--- tests/test_invalid.py
+++ tests/test_invalid.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 from datetime import time
 
-import pytest
+import unittest
 
 import toml_tools
 
 timezone = toml_tools._re  
 
-def test_invalid_type_nested():
-    with pytest.raises(TypeError):
-        toml_tools.dumps({"bytearr": bytearray()})
+class InvalidDataTests(unittest.TestCase):
+    def test_invalid_type_nested(self):
+        with self.assertRaises(TypeError):
+            toml_tools.dumps({"bytearr": bytearray()})
 
 
-def test_invalid_time():
-    offset_time = time(23, 59, 59, tzinfo=timezone.utc)
-    with pytest.raises(ValueError):
-        toml_tools.dumps({"offset time": offset_time})
+    def test_invalid_time(self):
+        offset_time = time(23, 59, 59, tzinfo=timezone.utc)
+        with self.assertRaises(ValueError):
+            toml_tools.dumps({"offset time": offset_time})
