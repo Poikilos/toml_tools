8eee6cda0945b55b7a4c2eb89d0d11ab438b6be8
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2023-05-01 10:43:36 +0100

    Moved test functions into unittest class

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,35 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from decimal import Decimal
+import unittest
 
 import toml_tools
 
-
-def test_decimal():
-    obj = {
-        "decimal-0": Decimal(0),
-        "decimal-pi": Decimal("3.14159"),
-        "decimal-inf": Decimal("inf"),
-        "decimal-minus-inf": Decimal("-inf"),
-        "decimal-nan": Decimal("nan"),
-    }
-    assert (
-        toml_tools.dumps(obj)
-        == """\
+class TypeWriterTests(unittest.TestCase):
+    def test_decimal(self):
+        obj = {
+            "decimal-0": Decimal(0),
+            "decimal-pi": Decimal("3.14159"),
+            "decimal-inf": Decimal("inf"),
+            "decimal-minus-inf": Decimal("-inf"),
+            "decimal-nan": Decimal("nan"),
+        }
+        self.assertEqual(
+            toml_tools.dumps(obj),
+            """\
 decimal-0 = 0
 decimal-pi = 3.14159
 decimal-inf = inf
 decimal-minus-inf = -inf
 decimal-nan = nan
 """
-    )
+        )
 
 
-def test_tuple():
-    obj = {"empty-tuple": (), "non-empty-tuple": (1, (2, 3))}
-    assert (
-        toml_tools.dumps(obj)
-        == """\
+    def test_tuple(self):
+        obj = {"empty-tuple": (), "non-empty-tuple": (1, (2, 3))}
+        self.assertEqual(
+            toml_tools.dumps(obj),
+            """\
 empty-tuple = []
 non-empty-tuple = [
     1,
@@ -39,4 +40,4 @@ non-empty-tuple = [
     ],
 ]
 """
-    )
+        )
