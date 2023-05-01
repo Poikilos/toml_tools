0f6844a1104caa73353e1edfed7dbb086bf4c149
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2023-05-01 10:14:18 +0100

    Refactored pytest function to unittest method

--- tests/test_write_file.py
+++ tests/test_write_file.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import toml_tools
+import unittest
 
-
-def test_dump(tmp_path):
-    toml_obj = {"testing": "test\ntest"}
-    path = tmp_path / "test.toml"
-    with open(path, "wb") as f:
-        toml_tools.dump(toml_obj, f)
-    assert path.read_bytes().decode() == 'testing = "test\\ntest"\n'
+class FileWriterTests(unittest.TestCase):
+    def test_dump(self, tmp_path):
+        toml_obj = {"testing": "test\ntest"}
+        path = tmp_path / "test.toml"
+        with open(path, "wb") as f:
+            toml_tools.dump(toml_obj, f)
+            
+        self.assertEqual(path.read_bytes().decode() == 'testing = "test\\ntest"\n')
